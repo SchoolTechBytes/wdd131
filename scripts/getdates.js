5159935636b1aeadfdad5f4d54c7fe61e3b516ca
SchoolTechBytes <nnixon@byupathway.edu>
2025-10-31 17:42:29 -0500

    date Time added

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,7 @@
 const today = new Date();
-const year = document.getElementById("currentyear").innerHTML = `${today.getFullYear()}`;
+const year = document.getElementById("currentyear").innerHTML = today.getFullYear();
 
 year.innerHTML = `${today.getFullYear()}`;
+
+const lastmodded = document.getElementById("lastModified").innerHTML += document.lastModified;
+
